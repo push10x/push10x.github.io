488fdbaab2e40edea4bc360eb474b9805b6cb134
mattheweq <repos@mattheweq.com>
2024-01-19 23:21:39 +0000

    Revert "configure paragraph links and add https to all links"
    This reverts commit 983ed93a7c788434f172ace7ac129b2a4efd9d38.

--- app.js
+++ app.js
@@ -7,43 +7,25 @@ document.addEventListener('DOMContentLoaded', function () {
     .then(data => {
 
       data.forEach(item => {
-        const {date, link, linkText, title, para, paraLink, paraLinkWord, highlightWords} = item
+        const {date, title, para, highlightWords, link, linkText} = item
 
         const dateHtml = date ? `<h3>${date}</h3>` : '';
         const titleHtml = title ? title : '';
         
-        function addHttps(x) {
-          if (!x.includes("http://") && !x.includes("https://")) {
-            return `https://${x}`;
-          } else {
-            return x.replace(/^http:\/\//, 'https://');
-          }
-        }
-
-        const linkUrlHtml = link ? addHttps(link) : '';
+        const linkUrlHtml = link ? link : '';
         const linkTextHtml = linkText ? linkText : '';
         const truncatedLinkText = linkTextHtml.length > 20 ? linkTextHtml.slice(0, 20) + '...' : linkTextHtml;
 
         const paraHtml = para ? para : '';
-        const paraLinkUrlHtml = paraLink ? addHttps(paraLink) : '';
-        const paraLinkWordHtml = paraLinkWord ? paraLinkWord : '';
         const highlightWordsHtml = highlightWords ? highlightWords : '';
-      
         let paraSpan = paraHtml;
 
-        if (paraHtml && (highlightWordsHtml || paraLinkWordHtml)) {
-          const wordsArray = (highlightWordsHtml + ' ' + paraLinkWordHtml).split(/\s+/);  // Split the words into an array
+        if (paraHtml && highlightWordsHtml) {
+          const wordsArray = highlightWordsHtml.split(/\s+/);  // Split the words into an array
           wordsArray.forEach(word => {
             if (word && paraSpan.includes(word)) {
-              // Add link to word
-              if (word === paraLinkWordHtml) {
-                const ctaHtml = ` <a href="${paraLinkUrlHtml}" target="_blank">${word}</a>`;
-                paraSpan = paraSpan.replace(new RegExp(word, 'ig'), ctaHtml);
-              } else {
-                // Highlight words
-                const ctaHtml = ` <span>${word}</span>`;
-                paraSpan = paraSpan.replace(new RegExp(word, 'ig'), ctaHtml);  // 'ig' for case-insensitive global match
-              }
+              const ctaHtml = ` <span>${word}</span>`;
+              paraSpan = paraSpan.replace(new RegExp(word, 'ig'), ctaHtml);  // 'ig' for case-insensitive global match
             }
           });
         }
@@ -53,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         displayElement.innerHTML += `<div>
           ${dateHtml}
-          <a href="${linkUrlHtml}" target="_blank">${truncatedLinkText}</a>
+          <a href=${linkUrlHtml} target="_blank">${truncatedLinkText}</a>
           <h2>${titleHtml}</h2>
           <p>${paraSpan}</p>
         </div>`;
